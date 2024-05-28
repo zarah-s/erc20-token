b51023626f0f114709ad58c1618617f6364a5c7c
victor samuel <81118182+zarah-s@users.noreply.github.com>
2024-05-28 16:00:17 +0100

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract CoitonERC20 is ERC20, Ownable {
-    constructor(address owner) ERC20("COITON", "CTN") Ownable(owner) {
-        _mint(owner, 100000000 * 10 ** decimals());
-    }
+contract MyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("COITON", "CTN")
+        Ownable(initialOwner)
+        ERC20Permit("COITON")
+    {}
 
-    function mintTo(address to, uint amount) external onlyOwner {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
